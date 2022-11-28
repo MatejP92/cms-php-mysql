4ddca4389e5735b2c85ad36c71281c3bd46b1d5a
Matej <110779759+MatejP92@users.noreply.github.com>
2022-11-28 12:46:29 +0100

    Update config.php

--- classes/config.php
+++ classes/config.php
@@ -4,6 +4,6 @@ class Config {
 
     const SMTP_HOST = "smtp.mailtrap.io";
     const SMTP_PORT = 2525;
-    const SMTP_USERNAME = "ad9be87457d593";
-    const SMTP_PASSWORD = "438dcbe7be48f5";
+    const SMTP_USERNAME = "<smtp-username>";
+    const SMTP_PASSWORD = "<smtp-password>";
 }
